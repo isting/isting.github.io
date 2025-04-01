685852e28e58ce04ecc0c7f0a7d391b258fd2998
liuting <ting.liu@acmeict.com>
2025-04-01 09:22:54 +0800

    feat: 新增51la统计

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -30,34 +30,9 @@ export const head: HeadConfig[] = [
     s.parentNode.insertBefore(hm, s);
   })();`,
   ],
-  // 页面访问量统计
-  // ['script', {}, `
-  // window.addEventListener('load', function() {
-  //   let oldHref = document.location.href, bodyDOM = document.querySelector('body');
-  //   const observer = new MutationObserver(function(mutations) {
-  //     if (oldHref != document.location.href) {
-  //       oldHref = document.location.href;
-  //       getPv()
-  //       window.requestAnimationFrame(function() {
-  //         let tmp = document.querySelector('body');
-  //         if(tmp != bodyDOM) {
-  //           bodyDOM = tmp;
-  //           observer.observe(bodyDOM, config);
-  //         }
-  //       })
-  //     }
-  //   });
-  //   const config = {
-  //     childList: true,
-  //     subtree: true
-  //   };
-  //   observer.observe(bodyDOM, config);
-  //   getPv()
-  // }, true);
-
-  // function getPv() {
-  //   xhr = new XMLHttpRequest();
-  //   xhr.open('GET', 'https://api.charles7c.top/blog/pv?pageUrl=' + location.href);
-  //   xhr.send();
-  // }`]
+  [
+    "script",
+    {},
+    `!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3LeFq2ipOcv7KHJ8",ck:"3LeFq2ipOcv7KHJ8"});`,
+  ],
 ];
