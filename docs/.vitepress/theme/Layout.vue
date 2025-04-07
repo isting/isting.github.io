7e1e132c9daf1ac605059852d835b3b6cd2bcc69
liuting <ting.liu@acmeict.com>
2025-04-07 14:07:22 +0800

    feat: 新增评论插件

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,49 @@
+<!--Layout.vue-->
+<template>
+  <Layout>
+    <template #doc-footer-before> </template>
+    <template #doc-after>
+      <div style="margin-top: 24px">
+        <Giscus
+          :key="page.filePath"
+          repo="isting/isting.github.io"
+          repo-id="R_kgDOONkPrQ"
+          category="Announcements"
+          category-id="DIC_kwDOONkPrc4Co2fN"
+          mapping="pathname"
+          strict="0"
+          reactions-enabled="1"
+          emit-metadata="0"
+          input-position="bottom"
+          lang="zh-CN"
+          crossorigin="anonymous"
+          :theme="isDark ? 'dark' : 'light'"
+        />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
+import Giscus from "@giscus/vue";
+import DefaultTheme from "vitepress/theme";
+import { watch } from "vue";
+import { inBrowser, useData } from "vitepress";
+
+const { isDark, page } = useData();
+
+const { Layout } = DefaultTheme;
+
+watch(isDark, (dark) => {
+  if (!inBrowser) return;
+
+  const iframe = document
+    .querySelector("giscus-widget")
+    ?.shadowRoot?.querySelector("iframe");
+
+  iframe?.contentWindow?.postMessage(
+    { giscus: { setConfig: { theme: dark ? "dark" : "light" } } },
+    "https://giscus.app"
+  );
+});
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,20 @@
 // .vitepress/theme/index.ts or .vitepress/theme/index.js
+import { h } from "vue";
 import Theme from "vitepress/theme";
 // import "./styles/vars.scss";
 // import "./styles/style.scss";
+import Layout from "./Layout.vue";
 
 // 只需添加以下一行代码，引入时间线样式
 import "vitepress-markdown-timeline/dist/theme/index.css";
 
 export default {
   ...Theme,
+  Layout: () => {
+    return h(Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
   enhanceApp(ctx) {
     Theme.enhanceApp(ctx);
   },
