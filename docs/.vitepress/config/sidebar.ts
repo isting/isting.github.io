0a23513d2a72f50d1e9a44d116fe4cf26c211fdc
“liuting-” <970888735@qq.com>
2025-11-10 13:50:20 +0800

    feat: 调整 sidebar 问题

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -6,35 +6,30 @@ import { generateSidebar } from "vitepress-sidebar";
 //   tag: string[];
 // }
 
-// const todayList: any = generateSidebar({
-//   documentRootPath: "/docs/today",
-//   collapseDepth: 3, // 折叠层级
-//   useTitleFromFileHeading: true, // 从文件标题提取名称
-//   collapsed: true, // 默认折叠sortMenusByName: true, // 排序
-//   // sortMenusOrderNumericallyFromTitle: true, // 数字排序
-//   // sortMenusByFrontmatterOrder: true, // 前端排序
-//   // sortMenusByFrontmatterDate: true, // 前端排序
-//   // sortMenusOrderByDescending: true, // 降序
-//   // sortFolderTo: "top", // 排序到顶部
-//   // rootGroupText: "今日总结", // 根目录名称
-//   // rootGroupLink: "/today/", // 根目录链接
-//   // rootGroupCollapsed: true, // 根目录默认折叠
-//   // // excludePattern: ["**/index.md", "**/README.md"], // 排除文件
-//   // excludePattern: ["**/README.md"], // 排除文件
-// });
-
-// console.log(777, JSON.stringify(todayList));
+// console.log(
+//   '侧边栏调试',
+//   JSON.stringify(
+//     generateSidebar({
+//       documentRootPath: "/docs",
+//       scanStartPath: "/interview",
+//       // resolvePath: "interview",
+//       // basePath: "interview",
+//     })
+//   )
+// );
 export const sidebar: DefaultTheme.Config["sidebar"] = {
   "/today": generateSidebar({
-    documentRootPath: "/docs/today",
+    documentRootPath: "/docs",
+    scanStartPath: "/today",
     collapseDepth: 3,
     useTitleFromFileHeading: true,
     collapsed: true,
   }) as any,
   "/interview": generateSidebar({
-    documentRootPath: "/docs/interview",
-    collapseDepth: 3,
+    documentRootPath: "/docs",
+    scanStartPath: "/interview",
     useTitleFromFileHeading: true,
+    collapseDepth: 3,
     collapsed: true,
   }) as any,
   // "/frontend/vue3": [],
---
+++ docs/_vue-hooks/utils.ts
@@ -0,0 +1,5 @@
+export const deepAddPrefix = (data, prefix: string) => {
+  if (!Array.isArray(data)) return [];
+
+  return; //
+};
